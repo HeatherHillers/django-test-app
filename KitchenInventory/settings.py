65c91cddb062153a4a6bd5da98e0b0d2a6014cfb
Heather Hillers <heather.hillers@erftverband.de>
2025-10-02 13:15:11 +0200

    asf

--- KitchenInventory/settings.py
+++ KitchenInventory/settings.py
@@ -75,19 +75,32 @@ WSGI_APPLICATION = 'KitchenInventory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'HHillers$kitchen_inventory',
-        'USER': 'HHillers',
-        'PASSWORD': 'I am a Rocket Man.',  # You need to set this to your actual MySQL password
-        'HOST': 'HHillers.mysql.eu.pythonanywhere-services.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+# Use SQLite for development/testing, MySQL for production
+import os
+
+if 'PYTHONANYWHERE_DOMAIN' in os.environ:
+    # Production MySQL settings for PythonAnywhere
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'HHillers$kitchen_inventory',
+            'USER': 'HHillers',
+            'PASSWORD': 'I am a Rocket Man.',
+            'HOST': 'HHillers.mysql.eu.pythonanywhere-services.com',
+            'PORT': '3306',
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+        }
     }
-}
+else:
+    # Development SQLite settings
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
