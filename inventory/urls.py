030a8a1094c740141070456bee2a22b299a631bc
Heather Hillers <heather.hillers@gmail.com>
2025-10-03 17:07:24 +0200

    change static path, fix path for inventory

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import Inventory, PurchaseOrderView, MenuView  #, MenuDetailView, MenuDeleteView
 urlpatterns = [
-    path('', Inventory.as_view(), name='inventory-list'),
+    path('', Inventory.as_view(), name='inventory'),
     path('purchase-orders/', PurchaseOrderView.as_view(), name='purchase-order-list'),
     path('menu/', MenuView.as_view(), name='menu-list'),
+    path('inventory/', Inventory.as_view(), name='inventory'),
     #path('menu/<int:pk>/', MenuDetailView.as_view(), name='menu-detail'),
     #path('menu/delete/<int:pk>/', MenuDeleteView.as_view(), name='menu-delete'),
 ]
