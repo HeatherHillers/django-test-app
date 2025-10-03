4318beec992eaa39d7bea94c7ac7d011a9c286d6
Heather Hillers <heather.hillers@gmail.com>
2025-10-03 17:38:04 +0200

    url fixes

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import Inventory, PurchaseOrderView, MenuView  #, MenuDetailView, MenuDeleteView
 urlpatterns = [
     path('', Inventory.as_view(), name='inventory'),
-    path('purchase-orders/', PurchaseOrderView.as_view(), name='purchase-order-list'),
-    path('menu/', MenuView.as_view(), name='menu-list'),
+    path('purchase-orders/', PurchaseOrderView.as_view(), name='purchases'),
+    path('menu/', MenuView.as_view(), name='menu'),
     path('inventory/', Inventory.as_view(), name='inventory'),
     #path('menu/<int:pk>/', MenuDetailView.as_view(), name='menu-detail'),
     #path('menu/delete/<int:pk>/', MenuDeleteView.as_view(), name='menu-delete'),
